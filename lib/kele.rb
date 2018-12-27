7af5a288f1e482a022da73318c5c136486198512
Pritchard45 <alexpritchard45@gmail.com>
2018-12-26 20:25:18 -0500

    initalize receives token

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,7 +4,7 @@ require 'json'
 
 class Kele
   include HTTParty
-  base_uri "https://www.bloc.io/api/v1/"
+  base_uri 'https://www.bloc.io/api/v1/'
 
   def initialize(email, password)
     response = self.class.post(api_url("sessions"), body: { email: email, password: password })
