b72b76cbb92071ff3f863696026955cf9dadd861
Pritchard45 <alexpritchard45@gmail.com>
2019-01-02 10:49:20 -0500

    can obtain availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,6 +6,7 @@ class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1/'
 
+
   def initialize(email, password)
     response = self.class.post(api_url("sessions"), body: { email: email, password: password })
     raise 'Invalid email or password' if response.code == 404
@@ -15,10 +16,14 @@ class Kele
 
   def get_me
     response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
-    @user = JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 
-  private
+    # mentor_id = 2292457
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    JSON.parse(response.body).to_a
+  end
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
