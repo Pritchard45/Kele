8c89fbc72d53931e9622e76ea6a1bdafcff15b97
Pritchard45 <alexpritchard45@gmail.com>
2019-01-07 10:18:01 -0500

    remaining checkpoints returns an array

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -20,6 +20,11 @@ module Roadmap
     JSON.parse(response.body)
   end
 
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get(api_url("enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"), headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
 
   private
 
